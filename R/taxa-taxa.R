a5dca701bcd8389ed92a6a2bb42e6e298d6ca565
Scott Chamberlain <myrmecocystus@gmail.com>
2016-09-08 16:48:44 -0700

    added new taxa classes, from #2 moved original classes into inst/ignore in case we want to bring any of that back

---
+++ R/taxa-taxa.R
@@ -0,0 +1,30 @@
+#' Make a set of many Taxon class objects
+#'
+#' @export
+#' @param ... Any number of object of class \code{Taxon}
+#' @return An \code{R6Class} object of class \code{Taxon}
+#' @examples
+#' (x <- taxon(
+#'   name = taxon_name("Poa annua"),
+#'   rank = taxon_rank("species"),
+#'   id = taxon_id(93036)
+#' ))
+#' taxa_(x, x, x)
+taxa_ <- function(...) {
+  structure(list(...), class = "taxa")
+}
+
+#' @export
+print.taxa <- function(x, ...) {
+  cat("<taxa>", "\n")
+  cat("  no. taxa: ", length(x), "\n")
+  for (i in seq_along(x[1:min(10, length(x))])) {
+    cat(
+      sprintf("  %s / %s / %s",
+              x[[i]]$name$name %||% "",
+              x[[i]]$rank$name %||% "",
+              x[[i]]$id$id %||% ""
+    ), "\n")
+  }
+  if (length(x) > 10) cat("  ...")
+}
