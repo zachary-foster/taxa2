be69c04493d342895f2dc7c020e94de1df1d6abd
Scott Chamberlain <myrmecocystus@gmail.com>
2017-06-27 14:44:19 -0700

    dontrun map_data eg for now as its not working fixes to pop egs doc

--- man-roxygen/pop_egs.R
+++ man-roxygen/pop_egs.R
@@ -17,21 +17,8 @@
 #' # With hierarchies class object
 #' # single taxonomic group
 #' invisible(lapply(ex_hierarchies, print))
-#' ex_hierarchies %>% pop(family) %>% lapply(., print) %>% invisible
+#' ex_hierarchies %>% pop(ranks(family)) %>% lapply(., print) %>% invisible
 #' ## more than one taxonomic group
 #' invisible(lapply(ex_hierarchies, print))
 #' ex_hierarchies %>% pop(ranks(family, genus)) %>% lapply(., print) %>%
 #'   invisible
-#'
-#' # Taxonomy class
-#' # ex_taxonomy$pop(ranks(family), NULL, NULL)
-#' # pop(ex_taxonomy, ranks(family))
-#' # pop(ex_taxonomy, ranks(family, genus))
-#' # taxon names
-#' # pop(ex_taxonomy, nms(Poa))
-#' # pop(ex_taxonomy, nms(Poaceae, Poa))
-#' # taxon ids
-#' # pop(ex_taxonomy, ids(4479))
-#' # pop(ex_taxonomy, ids(4479, 4544))
-#' # mixed: ids and names
-#' # pop(ex_taxonomy, ranks(family), ids(4544))
