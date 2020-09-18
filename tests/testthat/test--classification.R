20111663a293df13d567cd976eea9d544b6fe80f
Zachary S.L. Foster <zacharyfoster1989@gmail.com>
2020-09-18 13:25:56 -0700

    fixed classification tests after constructor refactor

--- tests/testthat/test--classification.R
+++ tests/testthat/test--classification.R
@@ -5,28 +5,28 @@ library(taxa)
 # Creating classification objects
 
 test_that("classification objects can be created from character input", {
-  x <- classification(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
+  x <- classification(c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+                      taxonomy(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
                         'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
-                      supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7),
-                      instances = c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2))
+                      supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7)))
   expect_equal(length(x), 12)
   expect_equal(class(x)[1], 'taxa_classification')
 })
 
 test_that("classification objects can be created from factor input", {
-  x <- classification(as.factor(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
-                                  'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos')),
-                      supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7),
-                      instances = c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2))
+  x <- classification(c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+                      taxonomy(as.factor(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
+                                           'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos')),
+                               supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7)))
   expect_equal(length(x), 12)
   expect_equal(class(x)[1], 'taxa_classification')
 })
 
 test_that("classification objects can be created with names", {
-  x <- classification(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
-                        'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
-                      supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7),
-                      instances = c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+  x <- classification(c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+                      taxonomy(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
+                                 'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
+                               supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7)),
                       .names = letters[1:12])
   expect_equal(length(x), 12)
   expect_equal(class(x)[1], 'taxa_classification')
@@ -55,10 +55,10 @@ test_that("classification objects can be created with a list of taxon info", {
 # Subsetting taxonomy objects with `[`
 
 test_that("classification objects can be subset with `[`", {
-  x <- classification(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
-                        'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
-                      supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7),
-                      instances = c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+  x <- classification(c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+                      taxonomy(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
+                                 'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
+                               supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7)),
                       .names = letters[1:12])
 
   expect_equal(length(x[2:5]), 4)
@@ -77,10 +77,10 @@ test_that("classification objects can be subset with `[`", {
 # Subsetting taxonomy objects with `[[`
 
 test_that("classification objects can be subset with `[[`", {
-  x <- classification(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
-                        'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
-                      supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7),
-                      instances = c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+  x <- classification(c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+                      taxonomy(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
+                                 'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
+                               supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7)),
                       .names = letters[1:12])
 
   expect_equal(length(x[[2]]), 1)
@@ -91,10 +91,10 @@ test_that("classification objects can be subset with `[[`", {
 })
 
 test_that("classification objects can only select one item with `[[`", {
-  x <- classification(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
-                        'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
-                      supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7),
-                      instances = c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+  x <- classification(c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+                      taxonomy(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
+                                 'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
+                               supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7)),
                       .names = letters[1:12])
 
   expect_error(x[[2:4]], 'attempt to select more than one element')
@@ -106,32 +106,32 @@ test_that("classification objects can only select one item with `[[`", {
 # Assigning values to classification objects with `[`
 
 test_that("Assigning values to classification objects with `[`", {
-  x <- classification(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
-                        'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
-                      supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7),
-                      instances = c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+  x <- classification(c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+                      taxonomy(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
+                                 'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
+                               supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7)),
                       .names = letters[1:12])
 
   x[2, 2:3] <- c('Ursidae', 'Ursus')
-
-
 })
 
 # classification objects can be combined with `c`
 
 test_that("classification objects can be combined with `c`", {
-  x <- classification(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
-                        'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
-                      supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7),
-                      instances = c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2))
-  y = classification(c('Carnivora', 'Felidae', 'Panthera', 'Kitty'), supertaxa = c(NA, 1, 2, 3), instances = 3:4)
+  x <- classification(c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2),
+                      taxonomy(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
+                                 'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos'),
+                               supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7)))
+  y = classification(3:4,
+                     taxonomy(c('Carnivora', 'Felidae', 'Panthera', 'Kitty'),
+                              supertaxa = c(NA, 1, 2, 3)))
 
   expect_equal(
     c(x, y),
-    classification(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
+    classification(c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2, 3, 9),
+                   taxonomy(c('Carnivora', 'Felidae', 'Panthera', 'Panthera leo',
                      'Panthera tigris', 'Ursidae', 'Ursus', 'Ursus arctos', 'Kitty'),
-                   supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7, 3),
-                   instances = c(3, 4, 4, 5, 5, 6, 8, 8, 2, 5, 6, 2, 3, 9))
+                   supertaxa = c(NA, 1, 2, 3, 3, 1, 6, 7, 3)))
   )
 })
 
